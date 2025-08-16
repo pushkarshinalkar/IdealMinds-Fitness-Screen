0713fc89d79cb8ef85fcd2167a7f0fadb6b61ded
Pushkar Shinalkar <126596736+pushkarshinalkar@users.noreply.github.com>
2025-08-16 20:29:24 +0530

    Update ActivityItem.kt

--- app/src/main/java/com/idealmind/testscreen/data/model/ActivityItem.kt
+++ app/src/main/java/com/idealmind/testscreen/data/model/ActivityItem.kt
@@ -4,10 +4,6 @@ package com.idealmind.testscreen.data.model
 import androidx.annotation.DrawableRes
 import java.util.UUID
 
-/**
- * Lightweight UI-only model for the “Recent Activity” feed.
- * You can enrich it later with real data if you add app logic.
- */
 data class ActivityItem(
     val id: String = UUID.randomUUID().toString(),
     val title: String,
